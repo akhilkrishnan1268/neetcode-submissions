918ae2d6acb03d602d86974a984bee2397f65e84
akhilkrishnan1268 <119856641+akhilkrishnan1268@users.noreply.github.com>
2026-06-10 11:06:36 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false;
+        }
+        let tmap = {};
+        let smap = {};
+        for(let i =0;i<s.length;i++){
+            smap[s[i]] = (smap[s[i]] ?? 0) + 1
+            tmap[t[i]] = (tmap[t[i]] ?? 0) + 1
+        }
+        for(const e in smap){
+            if(smap[e]!== tmap[e]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
